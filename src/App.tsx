2a0f581a98500eabfaec4c8ccd6a0064667e62fb
Botabay <tohgl49@gmail.com>
2023-05-07 23:13:44 +0600

    made a base htmlCss code for settingsWindow

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,42 @@ import { Button } from './components/Button'
 import { useEffect, useState } from 'react'
 
 export const App = () => {
-    const maxCount=5;
+    const countSettings = {start:0,max:0};
     const [countSt, setCountSt] = useState<number>(0);
-    useEffect(()=>{
-        console.log('useEffect');
-        window.localStorage.setItem('myCounter',countSt.toString())
-    },[countSt])
+    useEffect(() => {
+        window.localStorage.setItem('myCounter', countSt.toString())
+    }, [countSt])
     const toInc = () => {
-        setCountSt(countSt >= maxCount ? maxCount : countSt+1)
+        setCountSt(countSt >= countSettings.max ? countSettings.max : countSt + 1)
         //localStorage.setItem('myCounter',countSt.toString())
     }
     const toReset = () => {
         setCountSt(0);
         // localStorage.setItem('myCounter',countSt.toString())
     }
+    const toSet=()=>{
+
+    }
     return (
         <div className="App">
-            <CountView count={countSt} />
-            <div>
-                <Button name={'inc'} onClick={toInc} disabled={countSt===5}/>
-                <Button name={'reset'} onClick={toReset} disabled={countSt===0}/>
+            <div className='settingsWindow'>
+                <div className='maxField'>
+                    <label>max value:</label> <input type="number" />
+                </div>
+                <div className='startField'>
+                    <label>start value:</label> <input type="number" />
+                </div>
+                <div>
+                    <Button name={'set'} onClick={toSet} disabled={false} />
+                </div>
+            </div>
+            =======================
+            <div className='counterWindow'>
+                <CountView count={countSt} />
+                <div>
+                    <Button name={'inc'} onClick={toInc} disabled={countSt === 5} />
+                    <Button name={'reset'} onClick={toReset} disabled={countSt === 0} />
+                </div>
             </div>
         </div>
     );
