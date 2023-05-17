08bebfbb1a9878623bbb5b92cc465c036eb45644
Botabay <tohgl49@gmail.com>
2023-05-17 23:52:55 +0600

    a few refactoring

--- src/components/SettingsWindow.tsx
+++ src/components/SettingsWindow.tsx
@@ -11,27 +11,25 @@ type PropsType = {
 export const SettingsWindow = ({ callback }: PropsType) => {
     const [values, setValues] = useState<ValuesType>({ min: 0, max: 10 });
 
-    const toSet = () => {
-
-    }
+    const toSet = (e: ChangeEvent<HTMLInputElement>) => setValues({ ...values, max: Number(e.currentTarget.value) })
     return (
         <div className='settingsWindow'>
             <div className='maxField'>
                 <span>max value:</span>
                 <input
                     type="number"
-                    onChange={(e: ChangeEvent<HTMLInputElement>) => setValues({ ...values, max: Number(e.currentTarget.value) })}
+                    onChange={toSet}
                 />
             </div>
             <div className='startField'>
                 <span>min value:</span>
                 <input
                     type="number"
-                    onChange={(e: ChangeEvent<HTMLInputElement>) => setValues({ ...values, min: Number(e.currentTarget.value) })}
+                    onChange={toSet}
                 />
             </div>
             <div>
-                <Button name={'set'} onClick={toSet} disabled={false} />
+                <Button name={'set'} onClick={()=>callback()} disabled={false} />
             </div>
         </div>
     )
